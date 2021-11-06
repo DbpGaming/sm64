DbpGaming/sm64-portwithmychangesovergithubfuckingsuckingedition

odyssey movement
from https://github.com/TheGag96/sm64-port/tree/extended_moveset

src/game/mario_actions_automatic.c
@@ -547,6 +547,10 @@ s32 act_ledge_grab(struct MarioState *m) {
     s16 intendedDYaw = m->intendedYaw - m->faceAngle[1];
     s32 hasSpaceForMario = (m->ceilHeight - m->floorHeight >= 160.0f);
 
+    if (m->actionTimer == 0) {
+        m->spareFloat = m->forwardVel;
+    }
+
     if (m->actionTimer < 10) {
         m->actionTimer++;
     }
@@ -562,6 +566,16 @@ s32 act_ledge_grab(struct MarioState *m) {
     if ((m->input & INPUT_A_PRESSED) && hasSpaceForMario) {
         return set_mario_action(m, ACT_LEDGE_CLIMB_FAST, 0);
     }
+
+    if (m->actionTimer < 4 && (m->input & INPUT_B_PRESSED) && hasSpaceForMario && m->spareFloat >= 31.0f) {
+        mario_set_forward_vel(m, m->spareFloat + 5.0f);
+        m->vel[1] = 25.0f;
+#ifdef VERSION_SH
+        queue_rumble_data(5, 80);
+#endif
+        return set_mario_action(m, ACT_LEDGE_PARKOUR, 0);
+    }
+
 
     if (m->input & INPUT_STOMPED) {
         if (m->marioObj->oInteractStatus & INT_STATUS_MARIO_KNOCKBACK_DMG) {

DbpGaming/sm64

src/game/mario_actions_automatic.c
@@ -545,6 +545,10 @@ s32 act_ledge_grab(struct MarioState *m) {
     s16 intendedDYaw = m->intendedYaw - m->faceAngle[1];
     s32 hasSpaceForMario = (m->ceilHeight - m->floorHeight >= 160.0f);
 
+    if (m->actionTimer == 0) {
+        m->spareFloat = m->forwardVel;
+    }
+
     if (m->actionTimer < 10) {
         m->actionTimer++;
     }
@@ -560,6 +564,16 @@ s32 act_ledge_grab(struct MarioState *m) {
     if ((m->input & INPUT_A_PRESSED) && hasSpaceForMario) {
         return set_mario_action(m, ACT_LEDGE_CLIMB_FAST, 0);
     }
+
+    if (m->actionTimer < 4 && (m->input & INPUT_B_PRESSED) && hasSpaceForMario && m->spareFloat >= 31.0f) {
+        mario_set_forward_vel(m, m->spareFloat + 5.0f);
+        m->vel[1] = 25.0f;
+#ifdef ENABLE_RUMBLE
+        queue_rumble_data(5, 80);
+#endif
+        return set_mario_action(m, ACT_LEDGE_PARKOUR, 0);
+    }
+
 
     if (m->input & INPUT_STOMPED) {
         if (m->marioObj->oInteractStatus & INT_STATUS_MARIO_KNOCKBACK_DMG) {